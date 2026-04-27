cb57e3540c64c97401352999599dbb30978db318
UltraMax1623 <psg93367@gmail.com>
2026-04-27 18:49:30 +0530

    tranformations

--- decoder.py
+++ decoder.py
@@ -1,11 +1,19 @@
+# # imports
 from PIL import Image
 import numpy as np
 from scipy.ndimage import rotate
 import random
 from stegano import lsb
+# #
 
-# #open THE iamge resize to 300x300 -> convert to RGb image -> convert to greyscale ->
+
+# # inputs
 master_img = input("Enter master jpg image name(in current dir): ");
+encoded_img = input("Enter encoded PNG image name(in current dir): ");
+# #
+
+
+# # open THE iamge resize to 300x300 -> convert to RGb image -> convert to greyscale ->
 img_rgb = Image.open(master_img).convert("RGB")
 img_rgb = img_rgb.resize((300, 300))
 matrix_rgb = np.array(img_rgb)
@@ -15,10 +23,14 @@ G = matrix_rgb[:,:,1]
 B = matrix_rgb[:,:,2]
 
 matrix_manual_gray = (0.299 * R + 0.587 * G + 0.114 * B).astype(np.uint8)
-encoded_img = input("Enter encoded PNG image name(in current dir): ");
-secret = lsb.reveal(encoded_img)
-# secret = int(secret_str)
+# #
 
+
+# # decoding the encoded image
+secret = lsb.reveal(encoded_img)
+# #
+
+# # transforming master image based on decoded secret
 def decode_fixed(encoded):
     # Decode back from base36
     alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -28,39 +40,25 @@ def decode_fixed(encoded):
     return str(num)
 
 decoded = int(decode_fixed(secret))
-print(decoded)
-
 size = len(str(decoded))
-
 decoded_str = str(decoded)
+
 random_number = int(decoded_str[0:5])
-print(random_number)
-# 1. Rotate by (3MSB of random_number which ranges from (0-999)) with wrap mode
-transformed = rotate(matrix_manual_gray,random_number//100, reshape=True, mode='wrap')
 
-# # 2. Flip horizontally if(4th MSB is 1)
-if bool(int(str(random_number)[3])%2):
-    transformed = np.fliplr(transformed)
+digits = np.array([int(d) for d in str(random_number)])
+transformed = np.tile(digits, (90000 // digits.size + 1))[:90000]
+transformed = transformed.reshape(300, 300)
+# matrix tranformation
+transformed = np.dot(matrix_manual_gray,transformed)
+transformed = transformed%255
 
-# # 3. Flip verticallyz
-else:
-    transformed = np.flipud(transformed)
-
-# # 4. Transpose if(last bit is 1)
-if bool(int(str(random_number)[4])%2):
-    transformed = transformed.T
-
-img_final = Image.fromarray(transformed.astype(np.uint8))
-img_final.save("Decode_transformed.jpg")
-
+# # extracting message from master image based on index given in decoded secret
 hex_matrix = np.vectorize(lambda x: hex(int(x)))(transformed)
 
-#index = int(decoded_str[5:size-1])   # integer
-index_str = decoded_str[5:size]            # convert to string
-print("Index_str = ",index_str)
+index_str = decoded_str[5:size]
+
 # Step 1: Split into 6-digit chunks
 chunks = [index_str[i:i+6] for i in range(0, len(index_str), 6)]
-print("chunks : ",chunks)
 # Step 2: Convert each chunk to integer
 results = []
 for chunk in chunks:
@@ -79,8 +77,4 @@ for chunk in chunks:
 ascii_chars = [chr(int(val, 16)) for val in results]
 message = "".join(ascii_chars)
 
-print("Chunks:", chunks)
-print("Values:", results)
 print("Decoded message:", message)
-# ints = [int(chunk) for chunk in chunks]
-# print("ints : ",ints)
--- mapper.py
+++ mapper.py
@@ -1,12 +1,22 @@
-# #imports
+# # imports
 from PIL import Image
 import numpy as np
 from scipy.ndimage import rotate
 import random
 from stegano import lsb
+# #
 
-# #open THE iamge resize to 300x300 -> convert to RGb image -> convert to greyscale ->
+
+# # inputs
 master_img = input("Enter master jpg image name(in current dir): ");
+encoded_img = input("Enter PNG image name to be encoded(in current dir): ");
+message = input("Enter the messaage ")
+print("message :- ",message);
+# #
+
+
+
+# # open THE iamge resize to 300x300 -> convert to RGb image -> convert to greyscale ->
 img_rgb = Image.open(master_img).convert("RGB")
 img_rgb = img_rgb.resize((300, 300))
 matrix_rgb = np.array(img_rgb)
@@ -16,59 +26,32 @@ G = matrix_rgb[:,:,1]
 B = matrix_rgb[:,:,2]
 
 matrix_manual_gray = (0.299 * R + 0.587 * G + 0.114 * B).astype(np.uint8)
-
-# print("Manual grayscale matrix shape:", matrix_manual_gray.shape)
-# print("Matrix:\n", matrix_manual_gray)
-
-#img_manual_gray = Image.fromarray(matrix_manual_gray)
+# #
 
 
 # # Generate a random integer of fixed length to choose transformations on matrix_manual_gray matrix
 length = 5
 random_number = random.randint(10**(length-1), 10**length-1)
-print("Random number generated : ",random_number)
-# 1. Rotate by (3MSB of random_number which ranges from (0-999)) with wrap mode
-transformed = rotate(matrix_manual_gray,random_number//100, reshape=True, mode='wrap')
 
-# # 2. Flip horizontally if(4th MSB is 1)
-if bool(int(str(random_number)[3])%2):
-    transformed = np.fliplr(transformed)
+digits = np.array([int(d) for d in str(random_number)])
+transformed = np.tile(digits, (90000 // digits.size + 1))[:90000]
+transformed = transformed.reshape(300, 300)
+# matrix multiplication
+transformed = np.dot(matrix_manual_gray,transformed)
+# limiting to max value 255
+transformed = transformed%255
+# #
 
-# # 3. Flip verticallyz
-else:
-    transformed = np.flipud(transformed)
-
-# # 4. Transpose if(last bit is 1)
-if bool(int(str(random_number)[4])%2):
-    transformed = transformed.T
-
-# # Transformed matrix_manual_gray to transformed matrix -> now converting transformed matrix as image(just to see)
-img_final = Image.fromarray(transformed.astype(np.uint8))
-img_final.save("final_transformed.jpg")
-# print("Matrix:\n", transformed)
 
 # # Convert transformed int matrix to hex
 hex_matrix = np.vectorize(lambda x: hex(int(x)))(transformed)
+# #
 
-# #input the message to map
-message = input("Enter the messaage ")
-print(message);
-
-# #finding hex of message
+# # finding hex of message
 hex_array = [hex(ord(c)) for c in message];
+# #
 
-print("hex of message : ",hex_array)
-
-# results = {}
-# for key in hex_array:
-#     positions = np.argwhere(hex_matrix == key)
-#     if positions.size > 0:
-#         # Take the first occurrence only
-#         results[key] = positions[0].tolist()
-#     else:
-#         results[key] = None   
-
-# print(results)
+# # finding matching image hex and meesage hex and returning matched indexes and concatenating them to single string
 index_numbers = []
 for key in hex_array:
     positions = np.argwhere(hex_matrix == key)
@@ -78,15 +61,13 @@ for key in hex_array:
     else:
         index_numbers.append("000")
 
-#print(index_numbers)
-# Concatenate into one string
+
 final_index = "".join(index_numbers)
+# #
 
-print("hex matched idexes : ",index_numbers)
-print("hex matched idexes as int",final_index)
 
+# # encoding above string of numbers into encoded image(user inp) and save the new encoded iamge
 before_enc = str(random_number) + str(final_index)
-print("before Encoded:",before_enc )
 def encode_fixed(final_index, width=12):
     # Convert to integer
     num = int(final_index)
@@ -113,17 +94,11 @@ def decode_fixed(encoded):
 encoded = encode_fixed(before_enc, width=12)
 decoded = decode_fixed(encoded)
 
-print("Encoded:", encoded)   # Always 12 chars
-print("Decoded:", decoded)   # Original number
-
 message = str(encoded)
 
 # Hide the integer inside an image
-encoded_img = input("Enter PNG image name to be encoded(in current dir): ");
 secret_image = lsb.hide(encoded_img, message)
 secret_image.save("EncLSB.png")
 
-print("Integer hidden successfully in output.png")
-
-# revealed = lsb.reveal("EncLSB.png")
-# print("Revealed integer: ", revealed)
+print("Message hidden successfully in EncLSB.png")
+# # end
